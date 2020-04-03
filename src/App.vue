c2866aa2b705b5fdfaedbda1d8aec5fa97f6c318
amankr96 <aman.kr96@gmail.com>
2020-04-03 20:08:41 +0530

    Adding functionalities to Submit button

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
                                 type="text"
                                 id="email"
                                 class="form-control"
-                                v-model.lazy.trim="userData.email">                           
+                                v-model.lazy="userData.email">                           
                     </div>
                     <div class="form-group">
                         <label for="password">Password</label>
@@ -96,6 +96,7 @@
             </div>
             <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+             <hr>
                 <app-switch v-model="dataSwitch"></app-switch>
             </div>
             </div>
@@ -103,13 +104,14 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <button
-                            class="btn btn-primary">Submit!
+                            class="btn btn-primary"
+                             @click.prevent="submitted">Submit!
                     </button>
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
@@ -149,9 +151,14 @@ import Switch from './Switch.vue';
           gender: '',
           priorities: ['High', 'Medium', 'Low'],
           selectedPriority: '',
-          dataSwitch: true
-          
+          dataSwitch: true,
+          isSubmitted: false
         }
+      },
+      methods: {
+          submitted(){
+              this.isSubmitted=true;
+          }
       },
       components: {
           appSwitch: Switch
